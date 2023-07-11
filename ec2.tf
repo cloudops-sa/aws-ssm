a7e678215e5a506f67d4289709d7677fcbd0a664
cloudops-sa <sarvjeet.cloudops@gmail.com>
2023-07-11 18:44:29 -0400

    ec2 instances for ssm

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_instance" "ssm-instances" {
+  count = 4  # Number of instances to create
+
+  ami           = "ami-0ff8a91507f77f867"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"      # Replace with your desired instance type
+
+  tags = {
+    "Name" = "ssm-instance-${count.index+1}"
+    "team" = "dev-ops"
+  }
+}
